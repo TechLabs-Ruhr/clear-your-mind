825cc8f0318f9fe8513d4e30ffffd4c740518b43
Yousef <yhamadah@wwu.de>
2023-02-26 19:23:38 +0100

    Questions populated with dummy data. Submit button logs answers to console.

--- clear-your-mind/src/components/Questionnaire.js
+++ clear-your-mind/src/components/Questionnaire.js
@@ -1,63 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import {questions, survey} from './dummy';
+
+const options = ["Not at all", "Mildly, but it didn't bother me much", "Moderately - it wasn't pleasant at times", "Severely - it bothered me a lot"]
 
 export function Questionnaire() {
+    const [answers, setAnswers] = useState([]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Survey answers:", answers);
+      }
+
+    function handleOptionChange(questionIndex, optionIndex) {
+        const newAnswers = [...answers];
+        newAnswers[questionIndex] = optionIndex;
+        setAnswers(newAnswers);
+    }
+    
     return (<>
-    <div>
+    <div  className="container mt-3" style={{color:"#F6F7EB"}}>
         <h1>Questionnaire</h1>
+    
+    <div className="container mt-3">
+        <p>{survey[0].instructions}</p>
+    </div>
+        
+    <form onSubmit={handleSubmit}>
+      {questions.map((question, questionIndex) => (
+        <Row className='mb-3'>
+        <div key={questionIndex}>
+          <h3>{question.question}</h3>
+          {options.map((option, optionIndex) => (
+            <label className="container mt-3" key={optionIndex}>
+              <input
+                type="radio"
+                name={`question-${questionIndex}`}
+                value={option}
+                checked={answers[questionIndex] === optionIndex}
+                onChange={() => handleOptionChange(questionIndex, optionIndex)}
+              />
+              {option}
+            </label>
+          ))}
+        </div>
+        </Row>
+      ))}
+      <Button variant="primary" type="submit">Submit</Button>
+    </form> 
+
     </div>
 
-    <div className="container">
-        <Form style={{color:"white"}}>
-            <ol>
-                <li><p>Numbness or tingling</p></li>
-                    <Form.Group className="mb-3">
-                        <Form.Check type="radio" id="0" 
-                        name = "q1" label="Not at all"/>
-                        <Form.Check type="radio" id="1" 
-                        name = "q1" label="Mildly, but it doesn't bother me much"/>
-                        <Form.Check type="radio" id="2" 
-                        name = "q1" label="Moderately - it isn't pleasant at times"/>
-                        <Form.Check type="radio" id="3" 
-                        name = "q1" label="Severely - it bothers me a lot"/>
-                    </Form.Group>
-                <li><p>Feeling hot</p></li>
-                    <Form.Group className="mb-3">
-                        <Form.Check type="radio" id="0" 
-                        name = "q2" label="Not at all"/>
-                        <Form.Check type="radio" id="1" 
-                        name = "q2" label="Mildly, but it doesn't bother me much"/>
-                        <Form.Check type="radio" id="2" 
-                        name = "q2" label="Moderately - it isn't pleasant at times"/>
-                        <Form.Check type="radio" id="3" 
-                        name = "q2" label="Severely - it bothers me a lot"/>
-                    </Form.Group>
-                <li><p>Wobbliness in legs</p></li>
-                    <Form.Group className="mb-3">
-                        <Form.Check type="radio" id="0" 
-                        name = "q3" label="Not at all"/>
-                        <Form.Check type="radio" id="1" 
-                        name = "q3" label="Mildly, but it doesn't bother me much"/>
-                        <Form.Check type="radio" id="2" 
-                        name = "q3" label="Moderately - it isn't pleasant at times"/>
-                        <Form.Check type="radio" id="3" 
-                        name = "q3" label="Severely - it bothers me a lot"/>
-                    </Form.Group>
-            </ol>
-
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-    </div>
-    <div  className="container mt-3" style={{color:"white"}}>
-        <p>The total score is calculated by finding the sum of the 21 items.</p>
-        <ul>
-            <li>Score of 0-21 = low anxiety</li>
-            <li>Score of 22-35 = moderate anxiety</li>
-            <li>Score of 36 and above = potentially concerning levels of anxiety</li>
-        </ul>
-    </div>
     </> );
 }
---
+++ clear-your-mind/src/components/dummy.js
@@ -0,0 +1,42 @@
+export const questions = [
+    {
+        QID: 1,
+        questionID: 1,
+        question: "Numbness or tingling"
+    },
+    {
+        QID: 1,
+        questionID: 2,
+        question: "Feeling hot"
+    },
+    {
+        QID: 1,
+        questionID: 3,
+        question: "Wobbliness in legs"
+    },
+    {
+        QID: 1,
+        questionID: 4,
+        question: "Unable to relax"
+    },
+    {
+        QID: 1,
+        questionID: 5,
+        question: "Fear of worst happening"
+    },
+    {
+        QID: 1,
+        questionID: 6,
+        question: "Dizzy of lightheaded"
+    },
+    
+]
+
+export const survey = [
+    {
+    SurveyID: 1,
+    title: "Beck Anxiety Inventory",
+    instructions: "Below is a list of common symptoms of anxiety. Please carefully read each item in the list. Indicate how much you have been bothered by that symptom during the past month, including today, by circling the number in the corresponding space in the column next to each symptom.",
+    numberofQuestions: 8,
+    }
+]
