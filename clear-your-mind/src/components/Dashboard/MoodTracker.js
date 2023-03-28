bd15d4717940e54dc287c04f52c843da3ff7e2f0
Isztof <mariuszseget77@gmail.com>
2023-03-28 22:33:55 +0200

    Adding more style to the mood tracker

--- clear-your-mind/src/components/Dashboard/MoodTracker.js
+++ clear-your-mind/src/components/Dashboard/MoodTracker.js
@@ -5,6 +5,7 @@ import Footer from '../Footer'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { MDBRange } from 'mdb-react-ui-kit';
+import '../Dashboard/Dashboard.css'
 
 export default function MoodTracker() {
   return (
@@ -12,15 +13,26 @@ export default function MoodTracker() {
      <Header isLine={false}/>
      <div id='mainWrapper'>
         <main id='wrapper'> 
+        <h1 id='Greeting'>Track Your Mood</h1> 
+        <div id='description'>Drag the slider to assess your current
+         mood and click submit my mood </div>
+
         <MDBRange
         defaultValue={2.5}
         min='0'
-        max='5'
-        step='0.5'
+        max='6'
+        step='1'
         id='customRange3'
         label='Example range'
         />
         </main>
+        <div id='sliderLegend'> 
+          <div className='sliderLabel'>Terrible </div>
+          <div className='sliderLabel'>Bad</div>
+          <div className='sliderLabel'>Neutral</div>
+          <div className='sliderLabel'>Good</div>
+          <div className='sliderLabel'>Very Good</div>
+        </div>
      </div>   
      <div className='footer'>
     <Footer/>
