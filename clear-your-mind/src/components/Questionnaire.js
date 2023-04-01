ead64238850a4ca70cf9ccce7cd5866a262fff7f
Yousef <yhamadah@wwu.de>
2023-04-01 21:37:44 +0300

    Added all questions & results, moved to separate components

--- clear-your-mind/src/components/Questionnaire.js
+++ clear-your-mind/src/components/Questionnaire.js
@@ -1,52 +1,25 @@
 import React, {useState} from 'react';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-import {questions, survey} from './dummy';
-
-const options = ["Not at all", "Mildly, but it didn't bother me much", "Moderately - it wasn't pleasant at times", "Severely - it bothered me a lot"]
+import Questions from './Questions'
+import Results from './Results'
+import {questionnaire} from './dummy';
 
 export function Questionnaire() {
-    const [answers, setAnswers] = useState([]);
+  const [isComplete, setComplete] = useState(false);
+  const [result, setResult] = useState(0);
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log("Survey answers:", answers);
-      }
-
-    function handleOptionChange(questionIndex, optionIndex) {
-        const newAnswers = [...answers];
-        newAnswers[questionIndex] = optionIndex;
-        setAnswers(newAnswers);
-    }
+  const toggleVisibility = () => {
+    setComplete(!isComplete);
+  }
     
     return (<>
     <div  className="container mt-3" style={{color:"#F6F7EB"}}>
-        <h1>Questionnaire</h1>
-    <div className="container mt-3">
-        <p>{survey[0].instructions}</p>
-    </div> 
-    <form onSubmit={handleSubmit}>
-      {questions.map((question, questionIndex) => (
-        <div key={questionIndex}>
-        <Row className='mb-3'>
-          <h3>{question.question}</h3>
-          {options.map((option, optionIndex) => (
-            <label className="container mt-3" key={optionIndex}>
-              <input
-                type="radio"
-                name={`question-${questionIndex}`}
-                value={option}
-                checked={answers[questionIndex] === optionIndex}
-                onChange={() => handleOptionChange(questionIndex, optionIndex)}
-              />
-              {option}
-            </label>
-          ))}
-        </Row>
-        </div>
-      ))}
-      <Button variant="primary" type="submit">Submit</Button>
-    </form> 
+      <h1>{questionnaire[0].title}</h1>
+      {!isComplete && (
+        <Questions toggleVisibility={toggleVisibility} setResult={setResult} />
+      )}
+      {isComplete && (
+        <Results result={result} />
+      )}
     </div>
     </> );
 }
